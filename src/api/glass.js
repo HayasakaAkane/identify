4a0a6406ae1a8b257a303a4c1c67b88f50d87f87
ShikiNatsume <2261768355@qq.com>
2024-12-30 14:08:12 +0800

    测试接口

---
+++ src/api/glass.js
@@ -0,0 +1,3 @@
+// 眼镜相关请求
+import request from '@/utils/request'
+
--- src/views/home/test.vue
+++ src/views/home/test.vue
@@ -8,4 +8,29 @@
     <el-button type="warning">Warning</el-button>
     <el-button type="danger">Danger</el-button>
   </div>
+  <div>{{ res }}</div>
 </template>
+
+<script>
+import { defineComponent, onMounted, ref } from 'vue'
+import { GetAllAvatar } from '@/api/test';
+import { useAuthStore } from '@/pinia/modules/session';
+
+export default defineComponent({
+  setup() {
+    const authStore = useAuthStore()
+    const res = ref([])
+
+    onMounted(async () => {
+      const {code, data, msg} = await GetAllAvatar(authStore.sessionid)
+      if(code === 0) {
+        console.log(code, data, msg)
+        res.value = data
+      }
+    })
+
+    return { res }
+  },
+})
+</script>
+
