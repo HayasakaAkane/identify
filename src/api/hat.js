fc7d55f4c612c3240505abbd78e96c296df08afe
beiahi0 <1666825283@qq.com>
2024-12-31 16:44:50 +0800

    update

--- src/api/hat.js
+++ src/api/hat.js
@@ -15,3 +15,15 @@ export const GetHatAllarm = data => {
         },
     })
 }
+
+export const GetSnapPictures = data => {
+    return request({
+        url: '/api/json',
+        method: 'POST',
+        data: {
+            msg_id: '1036',
+            qry_len: 50,
+            lib_name: '飞翔的',
+        },
+    })
+}
