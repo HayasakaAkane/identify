78b590b7e2e48a99795b9fdc0ef8ecbbdf464403
beiahi0 <1666825283@qq.com>
2024-12-31 15:34:43 +0800

    alarm complete

--- src/api/glass.js
+++ src/api/glass.js
@@ -1,3 +1,18 @@
 // 眼镜相关请求
 import request from '@/utils/request'
 
+// 眼睛告警
+export const GetAllAvatar = data => {
+  return request({
+    url: '/api/json',
+    method: 'POST',
+    data: {
+      msg_id: '1036',
+      // msg_id: "1040",
+      qry_len: 50,
+    },
+    headers: {
+      sessionid: data,
+    },
+  })
+}
--- src/api/hat.js
+++ src/api/hat.js
@@ -1,3 +1,17 @@
 // 帽子相关请求
 import request from '@/utils/request'
 
+export const GetAllAvatar = data => {
+  return request({
+    url: '/api/json',
+    method: 'POST',
+    data: {
+      msg_id: '1036',
+      // msg_id: "1040",
+      qry_len: 50,
+    },
+    headers: {
+      sessionid: data,
+    },
+  })
+}
